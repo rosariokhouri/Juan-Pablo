be285e3ba6bc7a4b0c01166ee3952bca2f1938eb
Juan <juanp.torrese@gmail.com>
2020-02-20 10:54:55 -0300

    iit config --global user.email "you@example.com" Primer Commit

---
+++ src/main/java/edu/egg/Sumame/SumameApplication.java
@@ -0,0 +1,13 @@
+package edu.egg.Sumame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/egg/Sumame/SumameApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.egg.Sumame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
